2498423753d93487892113ebd1515a3c0b847ac1
Digneli Davila <digneli14@gmail.com>
2020-09-13 12:33:13 -0300

    Cargando parte conceptual de las funciones de los services y que metodos deberian usarse

--- src/main/java/ar/com/billetera/api/services/BilleteraService.java
+++ src/main/java/ar/com/billetera/api/services/BilleteraService.java
@@ -13,22 +13,22 @@ public class BilleteraService {
     /* Funcionalidad minima
 
         1. Metodo: Cargar saldo (recibe un importe y se le agrega a la cuenta)
-        1.1 -- Recibir un importe, se busca una billetera por id
+        1.1 -> Recibir un importe, se busca una billetera por id
         se busca una cuenta por la moneda
-        1.2 -- hacer transaccion
+        1.2 -> hacer transaccion
         Importe, salgo billetera
-        1.3 -- Actualizar el saldo de la billetera
+        1.3 -> Actualizar el saldo de la billetera
 
         2. Metodo: Enviar plata
-        2.1 -- recibir un importe, la moneda en la que va a estar ese importe
+        2.1 -> recibir un importe, la moneda en la que va a estar ese importe
         Info a nivel transaccion
         Metodos overload puede ser de mail a mail.
-        2.2 -- actualizar los saldos de las cuentas (a una se le suma y a la otra se le resta) 
-        2.3 -- Generar dos transacciones
+        2.2 -> actualizar los saldos de las cuentas (a una se le suma y a la otra se le resta) 
+        2.3 -> Generar dos transacciones
         Notificar
 
         3. Metodo: consultar saldo
-        3.1 --  recibir el id de la billetera y la moneda en la que esta la cuenta
+        3.1 ->  recibir el id de la billetera y la moneda en la que esta la cuenta
 
     */
     
