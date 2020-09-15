e6eb51e5263336cdc20dfe86ef286bbe50c399f5
Digneli Davila <digneli14@gmail.com>
2020-09-14 23:56:44 -0300

    actualizando conceptos en controllers

---
+++ src/main/java/ar/com/billetera/api/controllers/BilleteraController.java
@@ -0,0 +1,13 @@
+package ar.com.billetera.api.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BilleteraController {
+    /* Payload: request body
+    WEBmetodo 1:
+                    Consultar salgo: GET
+                    
+    
+    */
+}
